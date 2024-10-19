1dce593a0ba07bdd3ae20921dd25bad12df48bcf
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-10-20 00:04:29 +0200

    dirty hack, have a better idea ?

--- infrastructure/api-rest/src/test/java/fr/apiatlashub/server/infrastructure/apirest/ApiRestApplicationTests.java
+++ infrastructure/api-rest/src/test/java/fr/apiatlashub/server/infrastructure/apirest/ApiRestApplicationTests.java
@@ -1,10 +1,15 @@
 package fr.apiatlashub.server.infrastructure.apirest;
 
+import fr.apiatlashub.server.domain.api.SimpleApi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ApiRestApplicationTests {
+
+    @MockBean
+    private SimpleApi simpleApi;
 
     @Test
     void contextLoads() {}
--- justfile
+++ justfile
@@ -1,2 +1,8 @@
 spotless:
   mvn spotless:apply
+
+docker-build:
+  docker build -t api-atlas-hub-server:local -f docker/Dockerfile .
+
+docker-run-local:
+    docker run -p 9595:9595 api-atlas-hub-server:local
