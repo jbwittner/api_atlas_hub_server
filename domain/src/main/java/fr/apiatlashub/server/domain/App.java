f51b01f04d9c5b62663b5b16009960a0c8ee8873
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-10-19 22:45:35 +0200

    restored deleted entities just to be sure

---
+++ domain/src/main/java/fr/apiatlashub/server/domain/App.java
@@ -0,0 +1,8 @@
+package fr.apiatlashub.server.domain;
+
+/** Hello world! */
+public class App {
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ justfile
@@ -0,0 +1,2 @@
+spotless:
+  mvn spotless:apply
