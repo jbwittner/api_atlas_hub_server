dace91e34f2f250641c50aa55fbed892342fc314
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-10-20 14:32:54 +0200

    added security.

---
+++ infrastructure/spi-jpa/src/main/java/fr/apiatlashub/server/infrastructure/spijpa/SpiJpaApplication.java
@@ -0,0 +1,13 @@
+package fr.apiatlashub.server.infrastructure.spijpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiJpaApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SpiJpaApplication.class, args);
+    }
+
+}
---
+++ infrastructure/spi-jpa/src/test/java/fr/apiatlashub/server/infrastructure/spijpa/SpiJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.apiatlashub.server.infrastructure.spijpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
